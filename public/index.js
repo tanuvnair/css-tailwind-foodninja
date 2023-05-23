74f73fec7e9d87adbcd0e36f2c1786ee06a1dfe0
Tanuv Nair <tanuvnair@Rajnis-MacBook-Air.local>
2023-05-23 18:17:51 +0530

    Formatting changes

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ const burger = document.querySelector("#burger");
 const menu = document.querySelector("#menu");
 
 burger.addEventListener("click", () => {
-    if(menu.classList.contains("hidden")) {
+    if (menu.classList.contains("hidden")) {
         menu.classList.remove("hidden");
     } else {
         menu.classList.add("hidden");
